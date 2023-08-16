7c92dbf54fe1c3311b5e94320cfa2932a5d9a864
Changomango <schang09032003@gmail.com>
2023-08-16 15:26:35 +0800

    Feature: Completed First lesson of C language

---
+++ C Basics/HelloWorld/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+  //Simple Output
+  printf("Hello World!");
+}
---
+++ C Basics/HelloWorld/simplerecipe.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    //Simple Recipe
+    printf("2 Cups: All Purpose Flour\n"); //new line
+    printf("1 Cups: Unsalted Butter\t(Room Temperature)"); //tab space
+    printf("\n2/3 Cups: Granulated Sugar");
+}
