f8b3f523c926a481f9cd11a71fae41344d035cf4
Changomango <schang09032003@gmail.com>
2023-09-08 01:10:47 +0800

    Feature: Completed loop class

---
+++ C Control Flow/Loops and Errors/while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 0;
+  int square = 0;
+
+  // Write a while loop here:
+  while(i < 10) {
+    printf("%d\t%d\n", i, i*i);
+    i++;
+  }
+    while(i >= 0) {
+    printf("%d\t%d\n", i, i*i);
+    i--;
+  }
+
+}
